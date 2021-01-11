3b2616453f71714c0825b54cff3af1237ef81312
chuckboliver <patpumhak@gmail.com>
2021-01-11 22:32:20 +0700

    update

--- Tree/ฺbinaryTree.py
+++ Tree/ฺbinaryTree.py
@@ -4,12 +4,12 @@ class Node:
     """
     data node
     """
-    def __init__(self, data:int) -> None:
+    def __init__(self, data: int) -> None:
         self.key = data
         self.left = None
         self.right = None
 
-def inorder(node:Node) -> None:
+def inorder(node: Node) -> None:
     """
     depth first traversal for binary tree 
     """
@@ -19,7 +19,7 @@ def inorder(node:Node) -> None:
     print(node.key, end = " ")
     inorder(node.right)
 
-def insert(node:Node, key:int) -> Node:
+def insert(node: Node, key: int) -> Node:
     """
     insert element in binary tree
     """
@@ -42,17 +42,66 @@ def insert(node:Node, key:int) -> Node:
             queue.append(temp.right)
     return node
 
-def is_mirror(root:Node) -> bool:
+def delete(root: Node, key: int) -> Node:
+    """
+    delete element from binary tree and put deepest node to maintain  binary tree.
+    """
+    def __delete_deepest_util(root: Node, del_node: Node) -> None:
+        if root is del_node:
+            root = None
+            return
+        queue = deque()
+        queue.append(root)
+        while len(queue):
+            parent: Node = queue.popleft()
+            if parent.left is not None:
+                if parent.left is del_node:
+                    parent.left = None
+                    return
+                else:
+                    queue.append(parent.left)
+            if parent.right is not None:
+                if parent.right is del_node:
+                    parent.right = None
+                    return
+                else:
+                    queue.append(parent.right)
+    if root is None:
+        return None
+    if root.left == None and root.right == None:
+        if root.key == key:
+            return None
+        else:
+            return root
+    key_node: Node = None
+    temp: Node = None
+    queue = deque()
+    queue.append(root)
+    while len(queue):
+        temp: Node = queue.popleft()
+        if temp.key == key:
+            key_node = temp
+        if temp.left is not None:
+            queue.append(temp.left)
+        if temp.right is not None:
+            queue.append(temp.right)
+    if key_node is not None:
+        deepest_key = temp.key
+        __delete_deepest_util(root, temp)
+        key_node.key = deepest_key
+    return root
+
+def is_mirror(root: Node) -> bool:
     """
     check if tree is symmetric or not.
     """
-    def __is_mirror_check_util(left_node:Node, right_node:Node) -> bool:
+    def __is_mirror_check_util(left_node: Node, right_node: Node) -> bool:
     
         if left_node is None and right_node is None:
             return True
 
-        if not left_node is None and\
-            not right_node is None and\
+        if left_node is not None and\
+            right_node is not None and\
             left_node.key == right_node.key:
             return __is_mirror_check_util(left_node.left, right_node.right) and __is_mirror_check_util(left_node.right, right_node.left)
         
@@ -60,24 +109,24 @@ def is_mirror(root:Node) -> bool:
     
     return __is_mirror_check_util(root, root)
 
-def is_foldable(root:Node) -> bool:
+def is_foldable(root: Node) -> bool:
     """
     check if tree is foldable or not.
     """
-    def __is_foldable_util(left_node:Node, right_node:Node) -> bool:
+    def __is_foldable_util(left_node: Node, right_node: Node) -> bool:
 
         if left_node is None and right_node is None:
             return True
 
-        if not left_node is None and\
-            not right_node is None:
+        if left_node is not None and\
+             right_node is not None:
             return __is_foldable_util(left_node.left, right_node.right) and __is_foldable_util(left_node.right, right_node.left)
 
         return False
     
     return __is_foldable_util(root, root)
 
-def print_tree(node:Node, level:int = 0) -> None:
+def print_tree(node: Node, level: int = 0) -> None:
     """
     print binary tree
     """
@@ -87,9 +136,10 @@ def print_tree(node:Node, level:int = 0) -> None:
         print_tree(node.left, level + 1)
 
 if __name__ == "__main__":
-    root = None
+    root: Node = None
     for i in list(map(int, input("Enter key to insert : ").split())):
         root = insert(root, i)
+    delete(root, 9)
     print_tree(root)
     print(f"Symmetric : {is_mirror(root)}")
     print(f"Foldable : {is_foldable(root)}")
